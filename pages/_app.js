710c307db897650861d5436724fe100573ad1f5b
DanukaThimira <gamingvlab@gmail.com>
2022-10-27 14:14:37 -0700

    next-js-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import NavBar from '../components/NavBar'
 import '../styles/globals.css'
+import '../components/NavBar.css'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <SessionProvider session={pageProps.session}>
+    <NavBar />
+    <Component {...pageProps} />
+    </SessionProvider>
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/blog.js
@@ -0,0 +1,33 @@
+import { getSession, useSession } from "next-auth/react"
+
+function Blog({ data }) {
+  const {data: session} = useSession();
+  console.log({session})
+  return (
+    <div>
+        <h1>Blog - {data}</h1>
+    </div>
+  )
+}
+
+export default Blog
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: 'api/auth/signin?callbackUrl=http://localhost:3000/blog',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {
+      session,
+      data: session ? 'List of Personalized articles' : 'List of free blogs',
+    }
+  }
+}
